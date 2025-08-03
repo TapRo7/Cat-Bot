84d7223cefc56bda5729a74f79e905efda1666be
TapRo <talha.saleem7077@gmail.com>
2025-08-03 16:49:12 +0500

    Fix exec mode

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,7 @@ module.exports = {
         autorestart: true,
         watch: false,
         max_memory_restart: '1G',
-        instances: 1
+        instances: 1,
+        exec_mode: 'fork'
     }]
 };
