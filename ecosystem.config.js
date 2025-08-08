2b2d8a14d1ae9639b21bffbcf86135441c34a431
TapRo <talha.saleem7077@gmail.com>
2025-08-08 20:15:39 +0500

    Added custom log locations

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
     apps: [{
         name: 'Cat Bot',
@@ -5,6 +7,8 @@ module.exports = {
         autorestart: true,
         watch: false,
         max_memory_restart: '1G',
+        output: path.resolve(__dirname, 'logs', 'out.log'),
+        error: path.resolve(__dirname, 'logs', 'error.log'),
         instances: 1,
         exec_mode: 'fork'
     }]
