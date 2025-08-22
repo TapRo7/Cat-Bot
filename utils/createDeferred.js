2e731984d2ebe267531af33fadc100812bd2f89c
TapRo <talha.saleem7077@gmail.com>
2025-08-22 08:31:24 +0500

    Add defer helper function

---
+++ utils/createDeferred.js
@@ -0,0 +1,7 @@
+function createDeferred() {
+    let resolve, reject;
+    const promise = new Promise((res, rej) => { resolve = res; reject = rej; });
+    return { promise, resolve, reject };
+}
+
+module.exports = { createDeferred };
